aaaa3e8ba032c026cdddb55921077fe3ffc12c8d
quan <a@test.com>
2015-07-14 16:53:00 -0700

    Modify textView to show more details

--- app/src/main/java/com/example/android/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivityFragment.java
@@ -1,10 +1,12 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,15 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+
+        View fragmentView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Intent intent = getActivity().getIntent();
+        String message = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+        TextView textView = (TextView) fragmentView.findViewById(R.id.detail_text);
+        textView.append(message);
+
+        return fragmentView;
     }
 }
