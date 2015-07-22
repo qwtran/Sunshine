41a5aafb43312ac2190603e02be52484c8e68f56
quan <a@test.com>
2015-07-21 20:11:37 -0700

    Add logging to learn about activity lifecycle

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -18,8 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(LOG_TAG, "OnCreate");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(LOG_TAG, "OnStart");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(LOG_TAG, "OnResume");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(LOG_TAG, "OnPause");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(LOG_TAG, "OnStop");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(LOG_TAG, "OnDestroy");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
